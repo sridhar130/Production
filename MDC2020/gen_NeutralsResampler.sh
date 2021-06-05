d0edc3aca232de091a01fdd6920b517a160c98fc
David Nathan Brown <dave_brown@lbl.gov>
2021-06-05 10:21:27 -0500

    change to use reduced field maps

--- MDC2020/gen_NeutralsResampler.sh
+++ MDC2020/gen_NeutralsResampler.sh
@@ -1,4 +1,4 @@
-./generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1202 --description=NeutralsResampler --events-per-job=200000 --njobs=1000 --include JobConfig/beam/NeutralsResampler.fcl --auxinput=1:physics.filters.neutralsResampler.fileNames:Neutrals.txt
+generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1202 --description=NeutralsResampler --events-per-job=200000 --njobs=1000 --include JobConfig/beam/NeutralsResampler.fcl --auxinput=1:physics.filters.neutralsResampler.fileNames:Neutrals.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
