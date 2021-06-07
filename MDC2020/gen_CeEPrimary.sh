d31d51c0b4d4c0550a864d1e17dfcaca1f586d74
David Nathan Brown <dave_brown@lbl.gov>
2021-06-07 12:35:32 -0500

    updatgs

--- MDC2020/gen_CeEPrimary.sh
+++ MDC2020/gen_CeEPrimary.sh
@@ -1,4 +1,4 @@
-generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1203 --description=CeEndpoint --events-per-job=2000 --njobs=10 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:TargetStops.txt
+generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1203 --description=CeEndpoint --events-per-job=100000 --njobs=10 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:TargetStops.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
