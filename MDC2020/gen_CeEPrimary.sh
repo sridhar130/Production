992fedb0eb7984d47f8a542b9f73da9f24ea1b92
David Nathan Brown <dave_brown@lbl.gov>
2021-06-15 15:16:13 -0500

    Downstream adjustments for mixing with MuStopPileup

--- MDC2020/gen_CeEPrimary.sh
+++
@@ -1,9 +0,0 @@
-generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1203 --description=CeEndpoint --events-per-job=4000 --njobs=20 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:TargetStops.txt
-for dirname in 000 001 002 003 004 005 006 007 008 009; do
- if test -d $dirname; then
-  echo "found dir $dirname"
-  rm -rf CeEPrimary_$dirname
-  mv $dirname CeEPrimary_$dirname
- fi
-done
-
