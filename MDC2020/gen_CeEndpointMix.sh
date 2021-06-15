b59b4af51813a45c27566c15bcf9839300a7c06f
David Nathan Brown <dave_brown@lbl.gov>
2021-06-15 16:06:05 -0500

    fix small bug

--- MDC2020/gen_CeEndpointMix.sh
+++ MDC2020/gen_CeEndpointMix.sh
@@ -1,5 +1,5 @@
 generate_fcl --dsconf=MDC2020d --dsowner=brownd --override-outputs --auto-description=Mix --include JobConfig/mixing/MixPrimaryRun1.fcl \
---inputs=CeESkip.txt --merge-factor=1 \
+--inputs=CeEndpoint.txt --merge-factor=1 \
 --auxinput=1:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat.txt \
 --auxinput=1:physics.filters.BeamFlashMixer.fileNames:BeamFlashCat.txt \
 --auxinput=1:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat.txt 
