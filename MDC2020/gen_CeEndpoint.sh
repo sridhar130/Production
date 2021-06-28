2cec0f2dd0ba9ac2fed1d6709822c9da495bbefd
David Nathan Brown <dave_brown@lbl.gov>
2021-06-28 11:05:06 -0500

    Add scripts to split Beam into Mu and Ele parts

--- MDC2020/gen_CeEndpoint.sh
+++ MDC2020/gen_CeEndpoint.sh
@@ -1,4 +1,4 @@
-generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1203 --description=CeEndpoint --events-per-job=4000 --njobs=100 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusStopsCat.txt
+generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1210 --description=CeEndpoint --events-per-job=4000 --njobs=100 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusStopsCat.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
