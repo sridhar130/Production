41f25ae8058e7b21108c08d78433ea24ef5eeab4
David Nathan Brown <dave_brown@lbl.gov>
2021-06-29 21:28:49 -0500

    Genearlize generator scripts.  Update trigger_paths to include fixed indexing

--- MDC2020/gen_Mix.sh
+++ MDC2020/gen_Mix.sh
@@ -1,13 +1,13 @@
-generate_fcl --dsconf=MDC2020d --dsowner=brownd --override-outputs --auto-description=Mix --include JobConfig/mixing/MixPrimaryRun1.fcl \
---inputs=Cosmics.txt --merge-factor=1 \
+generate_fcl --dsconf="MDC2020$2" --dsowner=brownd --override-outputs --auto-description=Mix --include JobConfig/mixing/MixRun1.fcl \
+--inputs="$1$2.txt" --merge-factor=1 \
 --auxinput=1:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat.txt \
 --auxinput=1:physics.filters.BeamFlashMixer.fileNames:BeamFlashCat.txt \
 --auxinput=1:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat.txt 
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
-  rm -rf CosmicsMix_$dirname
-  mv $dirname CosmicsMix_$dirname
+  rm -rf "$1Mix_$dirname"
+  mv $dirname "$1Mix_$dirname"
  fi
 done
 
