7f2cfc4b9fdcaf3b2cb0d44befd7bf655196cb14
David Nathan Brown <dave_brown@lbl.gov>
2021-07-06 19:46:16 -0500

    Adapt mixing to 4 mixin streams

--- MDC2020/gen_MuStopPileup.sh
+++ MDC2020/gen_MuStopPileup.sh
@@ -1,4 +1,4 @@
-generate_fcl --dsconf=MDC2020d --dsowner=brownd --run-number=1205 --description=MuStopPileup --events-per-job=200000 --njobs=100 --include JobConfig/pileup/MuStopPileup.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuMinusTargetStops.txt
+generate_fcl --dsconf=MDC2020f --dsowner=brownd --run-number=1205 --description=MuStopPileup --events-per-job=200000 --njobs=100 --include JobConfig/pileup/MuStopPileup.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusTargetStops.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
