cc5f9731eb4140b49d6b59c591a0a63315529a3a
David Nathan Brown <dave_brown@lbl.gov>
2021-07-13 19:50:10 -0500

    Fix mixing scripts

--- MDC2020/gen_CeEndpoint.sh
+++ MDC2020/gen_CeEndpoint.sh
@@ -1,4 +1,4 @@
-generate_fcl --dsconf=MDC2020$1 --dsowner=brownd --run-number=1210 --description=CeEndpoint --events-per-job=4000 --njobs=100 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusStopsCat$1.txt
+generate_fcl --dsconf=MDC2020$1 --dsowner=brownd --run-number=1210 --description=CeEndpoint --events-per-job=4000 --njobs=100 --include JobConfig/primary/CeEndpoint.fcl --auxinput=1:physics.filters.TargetStopResampler.fileNames:MuminusTargetStops$1.txt
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
--- MDC2020/gen_Mix.sh
+++ MDC2020/gen_Mix.sh
@@ -1,6 +1,6 @@
-generate_fcl --dsconf="MDC2020$2" --dsowner=brownd --description="$1Mix" --include JobConfig/mixing/MixRun1.fcl \
+generate_fcl --dsconf="MDC2020$2" --dsowner=brownd --description="$1Mix$2" --include JobConfig/mixing/MixRun1.fcl \
 --inputs="$1$2.txt" --merge-factor=1 \
---auxinput=1:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCati$2.txt \
+--auxinput=1:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat$2.txt \
 --auxinput=1:physics.filters.EleBeamFlashMixer.fileNames:EleBeamFlashCat$2.txt \
 --auxinput=1:physics.filters.MuBeamFlashMixer.fileNames:MuBeamFlashCat$2.txt \
 --auxinput=1:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat$2.txt 
@@ -8,10 +8,11 @@ for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
   echo "found dir $dirname"
   rm -rf "$1Mix_$dirname"
-  mv $dirname "$1Mix_$dirname"
-  for file in $1Mix_$dirname/*.fcl; do
+  mv $dirname "$1Mix$2_$dirname"
+  for file in $1Mix$2_$dirname/*.fcl; do
     echo "editing file $file"
-    sed -i "s/description-/$1Mix/" $file
+    sed -i "s/MixTriggered/$1MixTriggered$2/" $file
+    sed -i "s/MixUntriggered/$1MixUntriggered$2/" $file
   done
  fi
 done
