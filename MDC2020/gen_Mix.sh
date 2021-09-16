9a41ced689d0c2e2d09cb8ebc4b9160202b733df
David Nathan Brown <dave_brown@lbl.gov>
2021-09-15 19:27:56 -0500

    Revert change of primary version

--- MDC2020/gen_Mix.sh
+++ MDC2020/gen_Mix.sh
@@ -40,7 +40,6 @@ samListLocations --defname="dts.mu2e.MuBeamFlashCat.$2$3.art" > MuBeamFlashCat$3
 samListLocations --defname="dts.mu2e.EleBeamFlashCat.$2$3.art" > EleBeamFlashCat$3.txt
 samListLocations --defname="dts.mu2e.NeutralsFlashCat.$2$3.art" > NeutralsFlashCat$3.txt
 samListLocations --defname="dts.mu2e.MuStopPileupCat.$2$3.art" > MuStopPileupCat$3.txt
-samListLocations --defname="dts.mu2e.$1.$2$3.art" > $1$3.txt
 # calucate the max skip from the dataset
 nfiles=`samCountFiles.sh "dts.mu2e.MuBeamFlashCat.$2$3.art"`
 nevts=`samCountEvents.sh "dts.mu2e.MuBeamFlashCat.$2$3.art"`
@@ -67,6 +66,7 @@ if [ $1 == "NoPrimary" ]; then
 elif [ $1 == "NoPrimaryPBISequence" ]; then
   echo '#include "Production/JobConfig/mixing/NoPrimaryPBISequence.fcl"' >> template.fcl
 else
+  samListLocations --defname="dts.mu2e.$1.$2$4.art" > $1$4.txt
   echo '#include "Production/JobConfig/mixing/Mix.fcl"' >> template.fcl
   echo '#include "Production/JobConfig/mixing/OneBB.fcl"' >> template.fcl  # number of booster batchs should be configuratble FIXME!
 fi
@@ -98,7 +98,7 @@ if [ $1 == "NoPrimary" ]; then
   --auxinput=1:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat$3.txt
 else
   generate_fcl --dsconf="$2$4" --dsowner=mu2e --description="$1Mix" --embed template.fcl \
-  --inputs="$1$3.txt" --merge-factor=1 \
+  --inputs="$1$4.txt" --merge-factor=1 \
   --auxinput=1:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat$3.txt \
   --auxinput=1:physics.filters.EleBeamFlashMixer.fileNames:EleBeamFlashCat$3.txt \
   --auxinput=1:physics.filters.MuBeamFlashMixer.fileNames:MuBeamFlashCat$3.txt \
