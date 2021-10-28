452d03ab40ea5702c77847f1acab603a17648667
David Nathan Brown <dave_brown@lbl.gov>
2021-10-28 11:25:47 -0500

    update printout

--- Scripts/datasetSummary.sh
+++ Scripts/datasetSummary.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #
 #  Summarize contents of a production dataset
-# $1 = user
-# $2 = production (ie MDC2020k)
-#
+# $1 = production (ie MDC2020k)
+# $2 = user (default mu2epro)
+user="mu2epro"
+if [[ ! -z $2 ]]; then
+  echo supplied user
+  user=$2
+fi
 # you must have run 'setup sam_web_client' (or 'setup dhtools') before running this script
 #
-samweb list-definitions --user=$1 | grep $2 | grep art > Datasets.txt
+samweb list-definitions --user=$user | grep $1 | grep art > Datasets.txt
 #
-echo "                        dataset                                     N files   totGB     N ev      MB/file   ev/file"
-echo "-------------------------------------------------------------------------------------------------------------------"
+echo "                        dataset                                    N ev  N files  totGB  MB/file  ev/file"
+echo "---------------------------------------------------------------------------------------------------------"
 while read DS || [[ -n $DS ]]; do
   TMP=`mktemp`
   samweb list-files --summary "dh.dataset=$DS and availability:anylocation" > $TMP
@@ -19,9 +23,9 @@ while read DS || [[ -n $DS ]]; do
     SZF=`cat $TMP | grep Total | awk '{printf "%d",$3/1000000.0/'$NN'}'`
     EC=`cat $TMP | grep Event | awk '{printf "%d",$3}'`
     ECF=`cat $TMP | grep Event | awk '{printf "%d",$3/'$NN'}'`
-    printf "%60s       %5d   %5d    %9d    %5d    %6d\n" $DS $NN $SZ $EC $SZF $ECF
+    printf "%60s  %9d  %5d  %5d  %5d  %7d\n" $DS $EC $NN $SZ $SZF $ECF
   else
-    printf "%60s       %5d   %5d    %9d    %5d    %6d\n" $DS $NN
+    printf "%60s  %5d  \n" $DS $NN
   fi
   rm -f $TMP
 done < Datasets.txt
