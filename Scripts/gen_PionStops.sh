049e8ea95d5d2eedfbac745306f4a192b1113f24
sophieMu2e <sophie@fnal.gov>
2021-12-02 10:01:57 -0600

    moved files

--- Scripts/gen_PionStops.sh
+++ Scripts/gen_PionStops.sh
@@ -1,4 +1,4 @@
-generate_fcl --dsconf=v0 --dsowner=sophie --description=PionStopSelector --include Production/JobConfig/pionbeam/PionStopSelector.fcl \
+generate_fcl --dsconf=v0 --dsowner=sophie --description=PionStopSelector --include Production/JobConfig/pileup/PionStopSelector.fcl \
 --inputs PiTargetStops.txt --merge-factor 1000
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
  if test -d $dirname; then
