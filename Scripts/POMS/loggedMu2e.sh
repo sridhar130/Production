9ab9dd436003012958c82b60f41e37a40f03085d
sophieMu2e <sophie@fnal.gov>
2023-01-19 03:45:43 -0600

    merge

--- Scripts/POMS/loggedMu2e.sh
+++ Scripts/POMS/loggedMu2e.sh
@@ -2,18 +2,14 @@
 echo mu2e "$@"
 pwd
 ls
-# file name is taken as input arguement:
 filename=`basename ${@: -1}` 
 echo $filename
 logFilename=${filename/cnf./log.}
 logExtension=${logFilename/.fcl/.txt}
 tbzFilename=${filename/cnf./bck.}
 tbzExtension=${tbzFilename/.fcl/.tbz}
-# write output to logExtension
 mu2e "$@" |& tee ${logExtension}
-# what does grep return? save as status
 mu2e_exit_status=${PIPESTATUS[0]}
-# tar files including logs
 tar -cvjf ${tbzExtension} ${logExtension}
 tar_exit_status=${?}
 exit ${mu2e_exit_status}
