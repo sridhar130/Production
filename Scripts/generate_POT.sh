8055b36c4587e81d6763ef1adb7203a9902a112e
sophieMu2e <sophie@fnal.gov>
2023-01-25 13:48:12 -0600

    check musing is present

--- Scripts/generate_POT.sh
+++ Scripts/generate_POT.sh
@@ -12,38 +12,41 @@ RUN=1202
 EVENTS=2000
 JOBS=1000
 DESC=POT
+
 # Function: Print a help message.
-usage() {                                 
+usage() {
   echo "Usage: $0 [ -c NAME ] [ -v VERSION ] [ -o OWNER ] [ -r RUN ] [ -e EVENTS ][ -j JOBS ][ -d DESC ]" 1>&2 
 }
+
 # Function: Exit with error.
-exit_abnormal() {                         
+exit_abnormal() {
   usage
   exit 1
 }
+
 # Loop: Get the next option;
-while getopts ":c:v:o:r:e:j:d:" options; do         
+while getopts ":c:v:o:r:e:j:d:" options; do
   case "${options}" in                    
-    c)                                  # If the option is n,
-      CAMPAIGN=${OPTARG}                      # set $CAMPAIGN to specified value.
+    c)                                    # If the option is n,
+      CAMPAIGN=${OPTARG}                  # set $CAMPAIGN to specified value.
       ;;
     v)                                    # If the option is v,
-      VERSION=${OPTARG}                     # Set $VERSION to specified value.
+      VERSION=${OPTARG}                   # Set $VERSION to specified value.
       ;;
     o)                                    # If the option is o,
       OWNER=${OPTARG}                     # Set $OWNER to specified value.
       ;;
     r)                                    # If the option is r,
-      RUN=${OPTARG}                     # Set $RUN to specified value.
+      RUN=${OPTARG}                       # Set $RUN to specified value.
       ;;
     e)                                    # If the option is e,
-      EVENTS=${OPTARG}                     # Set $EVENTS to specified value.
+      EVENTS=${OPTARG}                    # Set $EVENTS to specified value.
       ;;
     j)                                    # If the option is j,
-      JOBS=${OPTARG}                     # Set $JOBS to specified value.
+      JOBS=${OPTARG}                      # Set $JOBS to specified value.
       ;;
     d)                                    # If the option is d,
-      DESC=${OPTARG}                     # Set $DESC to specified value.
+      DESC=${OPTARG}                      # Set $DESC to specified value.
       ;;
     :)                                    # If expected argument omitted:
       echo "Error: -${OPTARG} requires an argument."
@@ -64,6 +67,7 @@ if [ -d "$DIR" ];
     echo "$DIR directory does not exist."
     exit 1
 fi
+
 # Run: run generate fcl with input from user
 generate_fcl --dsconf=${NAME}${VERSION} --dsowner=${OWNER} --run-number=${RUN} --events-per-job=${EVENTS} --njobs=${JOBS} --include Production/JobConfig/beam/POT.fcl --description=${DESC}
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
@@ -73,4 +77,3 @@ for dirname in 000 001 002 003 004 005 006 007 008 009; do
   mv $dirname POT_${NAME}${VERSION}_$dirname
  fi
 done
-
