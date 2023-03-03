8da5c77831171ddc61c5e3af39a99d7163913c98
David Nathan Brown <dave_brown@lbl.gov>
2023-03-03 13:15:53 -0600

    Synchronize gen_Mix with gen_Digitize

--- Scripts/gen_Digitize.sh
+++ Scripts/gen_Digitize.sh
@@ -1,36 +1,21 @@
 #!/usr/bin/bash
 #
+# generate fcl for digitizing simulated primary particles without pileup background
 # this script requires mu2etools, mu2efiletools and dhtools be setup
 # It also requires the SimEfficiencies for the beam campaign be entered in the database
 #
-PRIMARY="" # is the PRIMARY
-CAMPAIGN="" # production version
-PRIMARY_VERSION="" # version of the campaign that produced the primary
-DIGI_VERSION="" # version of the digi production
-TYPE="" # the kind of input stops (Muminus, Muplus, IPAMuminus, IPAMuplus, Piminus, Piplus, or Cosmic)
-MERGE="" #is the number of input collections to merge (merge factor)
-DIGITYPE="" #is the digitization type (OnSpill, OffSpill, NoField, Extracted)
-DB_PURPOSE="" # is the database purpose
-DB_VERSION="" # is the database version
-# The following can be overridden if needed
-OWNER=mu2e
-SAMOPT="-f "
-FIELD="Offline/Mu2eG4/geom/bfgeom_no_tsu_ps_v01.txt" #optional (for changing field map)
-
-
-usage() { echo "Usage: $0 [ --primary primary physics name ]
-  [ --campaign primary campaign name e.g. MDC2020 ]
-  [ --pver primary campaign versione ]
-  [ --dver digi campaign version ]
-  [ --type stopped particle type e.g. Muminus, Muplus, IPAMuminus, IPAMuplus, Piminus, Piplus, or Cosmic ]
-  [ --merge merge factor ]
+usage() { echo "Usage: $0 [ --primary primary physics process name ]
+  [ --campaign campaign name e.g. MDC2020 ]
+  [ --pver primary campaign version e.g 'p']
+  [ --over output campaign version e.g. 'v' ]
   [ --digitype  digitization type e.g. OnSpill, OffSpill, NoField, Extracted]
   [ --dbpurpose purpose of db e.g. perfect, startup, best  ]
   [ --dbversion db version ]
+  [ --merge (opt) merge factor, default 10 ]
   [ --owner (opt) default mu2e ]
-  [ --samopt (opt) Options to samListLocation default "-f" ]
-  [ --field (opt) override for special runs ]
-  e.g.  bash generate_Digitization.sh --primary CeEndpoint --campaign MDC2020--pver t --dver t --njobs 1000 --events 100 --merge 10 --digitype OnSpill --dbpurpose perfect --dbversion v1_0"
+  [ --samopt (opt) Options to samListLocation default "-f --schema=root" ]
+  [ --field (opt) default = DS +TSD, override for special runs ]
+  e.g.  Production/Scripts/gen_Digitize.sh --primary CeEndpoint --campaign MDC2020 --pver t --over t --digitype OnSpill --dbpurpose perfect --dbversion v1_0"
 }
 
 # Function: Exit with error.
@@ -39,23 +24,37 @@ exit_abnormal() {
   exit 1
 }
 
+# required configuration parameters
+PRIMARY=""
+CAMPAIGN=""
+PRIMARY_VERSION=""
+OUTPUT_VERSION=""
+DIGITYPE=""
+DBPURPOSE=""
+DBVERSION=""
+# The following options can be overridden if needed
+MERGE="10"
+OWNER=mu2e
+SAMOPT="-f --schema=root"
+FIELD="Offline/Mu2eG4/geom/bfgeom_no_tsu_ps_v01.txt" #optional (for changing field map)
+OUTDESC=""
 
 # Loop: Get the next option;
 while getopts ":-:" options; do
   case "${options}" in
     -)
       case "${OPTARG}" in
-        primary)
-          PRIMARY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
-          ;;
         campaign)
           CAMPAIGN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        primary)
+          PRIMARY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         pver)
           PRIMARY_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
-        dver)
-          DIGI_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+        over)
+          OUTPUT_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         merge)
           MERGE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
@@ -64,10 +63,10 @@ while getopts ":-:" options; do
           DIGITYPE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         dbpurpose)
-          DB_PURPOSE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          DBPURPOSE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         dbversion)
-          DB_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          DBVERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         owner)
           OWNER=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
@@ -79,7 +78,7 @@ while getopts ":-:" options; do
           FIELD=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         desc)
-          DESC=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          OUTDESC=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         esac;;
     :)                                    # If expected argument omitted:
@@ -87,34 +86,35 @@ while getopts ":-:" options; do
       exit_abnormal                       # Exit abnormally.
       ;;
     *)                                    # If unknown (any other) option:
+      echo "Unknown option ${OPTARG}"
       exit_abnormal                       # Exit abnormally.
       ;;
     esac
 done
 
-# basic tests: expand to all required arguments TODO
-if [[ ${CAMPAIGN} == "" ]]; then
-  echo "No campaign specified: exit"
-  exit 1
+# basic tests
+if [[ ${CAMPAIGN} == ""  || ${PRIMARY} == "" || ${PRIMARY_VERSION} == "" || ${OUTPUT_VERSION} == "" || ${DIGITYPE} == "" || ${DBVERSION} == "" || ${DBPURPOSE} == "" ]]; then
+  echo "Missing arguments"
+  exit_abnormal
 fi
 
-DESC=${PRIMARY}.${CAMPAIGN}${PRIMARY_VERSION} # can override if more detailed tag is needed
+PRIMARYCONF=${CAMPAIGN}${PRIMARY_VERSION}
 
 # Test: run a test to check the SimJob for this campaign verion exists
-DIR=/cvmfs/mu2e.opensciencegrid.org/Musings/SimJob/${CAMPAIGN}${DIGI_VERSION}
+DIR=/cvmfs/mu2e.opensciencegrid.org/Musings/SimJob/${CAMPAIGN}${OUTPUT_VERSION}
 if [ -d "$DIR" ];
   then
     echo "Musing $DIR exists."
   else
     echo "Musing $DIR does not exist."
-    exit 1
+    exit_abnormal
 fi
 
 
 if [[ "${DIGITYPE}" == "Extracted" || "${DIGITYPE}" == "NoField" ]]; then
   if [[ "${PRIMARY}" != *"${DIGITYPE}"* ]]; then
     echo "PRIMARY ${PRIMARY} doesn't match digitization type ${DIGITYPE}; aborting"
-    return 1
+    exit_abnormal
   else
     # no need for redundant labels
     digout=$PRIMARY
@@ -123,13 +123,13 @@ if [[ "${DIGITYPE}" == "Extracted" || "${DIGITYPE}" == "NoField" ]]; then
 else
   if [[ "${PRIMARY}" == *"Extracted"* || "${PRIMARY}" == *"NoField"* ]]; then
     echo "PRIMARY ${PRIMARY} incompatible with digitization type ${DIGITYPE}; aborting"
-    return 1
+    exit_abnormal
   fi
 fi
-echo "Generating digitization scripts for campaign ${CAMPAIGN} primary $PRIMARY version ${PRIMARY_VERSION} musing version ${DIGI_VERSION} digitzation type ${DIGITYPE} database purpose, version  ${DB_PURPOSE} ${DB_VERSION}"
+echo "Generating digitization scripts for campaign ${CAMPAIGN} primary $PRIMARY version ${PRIMARY_VERSION} musing version ${OUTPUT_VERSION} digitzation type ${DIGITYPE} database purpose, version  ${DBPURPOSE} ${DBVERSION}"
 
 rm -f digitize.fcl
-samListLocations ${SAMOPT} --schema=root --defname="dts.mu2e.${DESC}.art" > ${DESC}.txt
+samListLocations ${SAMOPT} --defname="dts.mu2e.${PRIMARY}.${PRIMARYCONF}.art" > ${PRIMARY}.txt
 echo \#include \"Production/JobConfig/digitize/Digitize.fcl\" >> digitize.fcl
 echo \#include \"Production/JobConfig/digitize/${DIGITYPE}.fcl\" >> digitize.fcl
 # turn off streams according to the digitization type.
@@ -146,13 +146,13 @@ else
   echo outputs.UntriggeredOutput.fileName: \"dig.owner.${PRIMARY}${DIGITYPE}Untriggered.version.sequencer.art\" >> digitize.fcl
 fi
 # setup database access for digi parameters
-echo services.DbService.purpose: ${CAMPAIGN}_${DB_PURPOSE} >> digitize.fcl
-echo services.DbService.version: ${DB_VERSION} >> digitize.fcl
+echo services.DbService.purpose: ${CAMPAIGN}_${DBPURPOSE} >> digitize.fcl
+echo services.DbService.version: ${DBVERSION} >> digitize.fcl
 echo services.DbService.verbose : 2 >> digitize.fcl
 echo "services.GeometryService.bFieldFile : \"${FIELD}\"" >> digitize.fcl
-
-generate_fcl --dsconf="${CAMPAIGN}${DIGI_VERSION}_${DB_PURPOSE}_${DB_VERSION}" --dsowner=${OWNER} --description="${PRIMARY}${DIGITYPE}" --embed digitize.fcl \
-  --inputs="${DESC}.txt" --merge-factor=${MERGE}
+OUTCONF=${CAMPAIGN}${OUTPUT_VERSION}_${DBPURPOSE}_${DBVERSION}
+generate_fcl --dsconf="${OUTCONF}" --dsowner=${OWNER} --description="${PRIMARY}${DIGITYPE}" --embed digitize.fcl \
+  --inputs="${PRIMARY}.txt" --merge-factor=${MERGE}
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
   if test -d $dirname; then
     echo "found dir $dirname"
--- Scripts/gen_Mix.sh
+++ Scripts/gen_Mix.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/bash
+# generate fcl for running mixing, a specialization of OnSpill digitization with pileup backgrounds
+# this script requires mu2etools, mu2efiletools and dhtools be setup
+# It also requires the SimEfficiencies for the beam campaign be entered in the database
 # Function: Print a help message.
-usage() {
-  echo "Usage: $0 [ --primary NAME ] 
-  [ --mcamp mixing campaign name ]
-  [ --pcamp primary campaign name ]
+usage() { echo "Usage: $0 [ --primary primary physics process name ]
+  [ --campaign campaign name e.g. MDC2020 ]
+  [ --pver primary campaign version e.g 'r']
+  [ --mver mixin (input) campaign version e.g. 'p' ]
+  [ --over output campaign version e.g. 'v' ]
+  [ --pbeam proton beam intensity e.g. 1BB (one Booster Batch), 2BB, Low, or Seq (sequential)]
   [ --dbpurpose purpose of db e.g. perfect, startup, best  ]
   [ --dbversion db version ]
-  [ --nbb beam mode ]
-  [ --early (opt) for early run ]
+  [ --early (opt) for early digitization.  Intensity will be set to 'Low' ]
   [ --merge (opt) merge factor, default 10 ]
-  [ --neutmix (opt) neutral mixing factor ]
-  [ --elemix (opt) electron mixing factor ]
-  [ --mustopmix (opt) electron mixing factor ]
-  [ --mubeammix (opt) electron mixing factor ]" 1>&2
+  [ --owner (opt) default mu2e ]
+  [ --samopt (opt) Options to samListLocation default "-f --schema=root" ]
+  [ --field (opt) default = DS +TSD, override for special runs ]
+  [ --neutmix (opt) # of neutral pileup files ]
+  [ --elemix (opt) # of electron pileup files ]
+  [ --mustopmix (opt) # of mustop daughter pileup files ]
+  [ --mubeammix (opt) # of mubeam pileup files ]
+    e.g.  Production/Scripts/gen_Mix.sh --primary CeEndpoint --campaign MDC2020 --pver t --mver p --over v --beam 1BB --dbpurpose perfect --dbversion v1_0"
 }
 
 # Function: Exit with error.
@@ -21,12 +29,15 @@ exit_abnormal() {
   exit 1
 }
 
-PRIMARY="" # e.g. CeEndpoint
-MIXCAMP="" # e.g MDC2020p
-PCAMP=""  # e.g. MDC2020v
-DBPURPOSE="" # e.g. perfect
-DBVERSION="" # e.g. v1_0
-NBB="" # e.g.1BB
+# required configuration parameters
+PRIMARY=""
+CAMPAIGN=""
+PRIMARY_VERSION=""
+OUTPUT_VERSION=""
+MIXIN_VERSION=""
+DBPURPOSE=""
+DBVERSION=""
+PBEAM=""
 EARLY=""
 MERGE=10
 OWNER=mu2e
@@ -34,121 +45,163 @@ NEUTNMIXIN=50
 ELENMIXIN=25
 MUSTOPNMIXIN=2
 MUBEAMNMIXIN=1
+SAMOPT="-f --schema=root"
+FIELD="Offline/Mu2eG4/geom/bfgeom_no_tsu_ps_v01.txt"
 
 
 # Loop: Get the next option;
 while getopts ":-:" options; do
-  case "${options}" in 
-    -)                                   
+  case "${options}" in
+    -)
       case "${OPTARG}" in
+        campaign)
+          CAMPAIGN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
         primary)
-          PRIMARY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))      
+          PRIMARY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
-        mcamp)            
-          MIXCAMP=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))      
+        pver)
+          PRIMARY_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
-        pcamp)            
-          PCAMP=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                
+        mver)
+          MIXIN_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        over)
+          OUTPUT_VERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        pbeam)
+          PBEAM=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         dbpurpose)
-          DBPURPOSE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                   
+          DBPURPOSE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         dbversion)
-          DBVERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                  
-          ;;
-        nbb)
-          NBB=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                  
+          DBVERSION=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         early)
-          EARLY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                   
+          EARLY=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         merge)
-          MERGE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                   
+          MERGE=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         owner)
-          OWNER=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
+          OWNER=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        samopt)
+          SAMOPT=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         neutmix)
-          NEUTNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
+          SAMOPT=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        field)
+          FIELD=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        neutmix)
+          NEUTNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         elemix)
-          ELENMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
+          ELENMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         mustopmix)
-          MUSTOPNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
+          MUSTOPNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
           ;;
         mubeammix)
-          MUBEAMNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))                     
-          ;;                                   
-        esac;;             
+          MUBEAMNMIXIN=${!OPTIND} OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        esac;;
     :)                                    # If expected argument omitted:
       echo "Error: -${OPTARG} requires an argument."
       exit_abnormal                       # Exit abnormally.
       ;;
     *)                                    # If unknown (any other) option:
+      echo "Unknown option ${OPTARG}"
       exit_abnormal                       # Exit abnormally.
       ;;
     esac
 done
 
 
-if [[ ${EARLY} -eq "Early" ]]; then
+# basic tests
+if [[ ${CAMPAIGN} == ""  || ${PRIMARY} == "" || ${PRIMARY_VERSION} == "" || ${MIXIN_VERSION} == "" || ${OUTPUT_VERSION} == "" ||${PBEAM} == "" || ${DBVERSION} == "" || ${DBPURPOSE} == "" ]]; then
+  echo "Missing arguments: exit"
+  exit_abnormal
+fi
+
+
+if [[ ${EARLY} == "Early" ]]; then
+  echo "Early mixing selected"
   NEUTNMIXIN=1
   ELENMIXIN=1
   MUSTOPNMIXIN=1
   MUBEAMNMIXIN=1
-  NBB="Low"
+  PBEAM="Low"
 fi
 
-OUTCONF=${PCAMP}_${DBPURPOSE}_${DBVERSION}
-MIXOUT=${PRIMARY}Mix${NBB}${EARLY}
+# define configuration fields for primary, mixin, and output
+
+PRIMARYCONF=${CAMPAIGN}${PRIMARY_VERSION}
+MIXINCONF=${CAMPAIGN}${MIXIN_VERSION}
+OUTCONF=${CAMPAIGN}${OUTPUT_VERSION}_${DBPURPOSE}_${DBVERSION}
+# output collection description root
+OUTDESC=${PRIMARY}Mix${PBEAM}${EARLY}
 
 # consistency check: cannot mix Extracted or NoField data
 if [[ "${PRIMARY}" == *"Extracted" || "${PRIMARY}" == *"NoField" ]]; then
   echo "Primary ${PRIMARY} incompatible with mixing; aborting"
-  return 1
+  exit_abnormal
 fi
 
-echo "Generating mixing scripts for ${PRIMARY} conf ${PCAMP} mixin conf ${MIXCAMP} output conf ${OUTCONF} database purpose, version ${DBPURPOSE} ${DBVERSION} ${EARLY} with ${NBB} proton intensity"
+# Test: run a test to check the SimJob for this campaign verion exists
+DIR=/cvmfs/mu2e.opensciencegrid.org/Musings/SimJob/${CAMPAIGN}${OUTPUT_VERSION}
+if [ -d "$DIR" ];
+  then
+    echo "Musing $DIR exists."
+  else
+    echo "Musing $DIR does not exist."
+    exit_abnormal
+fi
 
- create the mixin input lists.  Note there is no early MuStopPileup
-samweb list-file-locations --schema=root --defname="dts.mu2e.${EARLY}MuBeamFlashCat.${MIXCAMP}.art"  | cut -f1 > MuBeamFlashCat${MIXCAMP}.txt
-samweb list-file-locations --schema=root --defname="dts.mu2e.${EARLY}EleBeamFlashCat.${MIXCAMP}.art"  | cut -f1 > EleBeamFlashCat${MIXCAMP}.txt
-samweb list-file-locations --schema=root --defname="dts.mu2e.${EARLY}NeutralsFlashCat.${MIXCAMP}.art"  | cut -f1 > NeutralsFlashCat${MIXCAMP}.txt
-samweb list-file-locations --schema=root --defname="dts.mu2e.MuStopPileupCat.${MIXCAMP}.art"  | cut -f1 > MuStopPileupCat${MIXCAMP}.txt
+
+echo "Generating mixing scripts for ${PRIMARY} conf ${PRIMARYCONF} mixin conf ${MIXINCONF} output config, description ${OUTCONF} ${OUTDESC}"
+
+# create the mixin input lists.  Note there is no early MuStopPileup
+samListLocations ${SAMOPT} --defname="dts.mu2e.${EARLY}MuBeamFlashCat.${MIXINCONF}.art"  > MuBeamFlashCat${MIXINCONF}.txt
+samListLocations ${SAMOPT} --defname="dts.mu2e.${EARLY}EleBeamFlashCat.${MIXINCONF}.art"  > EleBeamFlashCat${MIXINCONF}.txt
+samListLocations ${SAMOPT} --defname="dts.mu2e.${EARLY}NeutralsFlashCat.${MIXINCONF}.art" > NeutralsFlashCat${MIXINCONF}.txt
+samListLocations ${SAMOPT} --defname="dts.mu2e.MuStopPileupCat.${MIXINCONF}.art" > MuStopPileupCat${MIXINCONF}.txt
 
 # calucate the max skip from the dataset
-nfiles=`samCountFiles.sh "dts.mu2e.MuBeamFlashCat.${MIXCAMP}.art"`
-nevts=`samCountEvents.sh "dts.mu2e.MuBeamFlashCat.${MIXCAMP}.art"`
+nfiles=`samCountFiles.sh "dts.mu2e.MuBeamFlashCat.${MIXINCONF}.art"`
+nevts=`samCountEvents.sh "dts.mu2e.MuBeamFlashCat.${MIXINCONF}.art"`
 let nskip_MuBeamFlash=nevts/nfiles
-nfiles=`samCountFiles.sh "dts.mu2e.EleBeamFlashCat.${MIXCAMP}.art"`
-nevts=`samCountEvents.sh "dts.mu2e.EleBeamFlashCat.${MIXCAMP}.art"`
+nfiles=`samCountFiles.sh "dts.mu2e.EleBeamFlashCat.${MIXINCONF}.art"`
+nevts=`samCountEvents.sh "dts.mu2e.EleBeamFlashCat.${MIXINCONF}.art"`
 let nskip_EleBeamFlash=nevts/nfiles
-nfiles=`samCountFiles.sh "dts.mu2e.NeutralsFlashCat.${MIXCAMP}.art"`
-nevts=`samCountEvents.sh "dts.mu2e.NeutralsFlashCat.${MIXCAMP}.art"`
+nfiles=`samCountFiles.sh "dts.mu2e.NeutralsFlashCat.${MIXINCONF}.art"`
+nevts=`samCountEvents.sh "dts.mu2e.NeutralsFlashCat.${MIXINCONF}.art"`
 let nskip_NeutralsFlash=nevts/nfiles
-nfiles=`samCountFiles.sh "dts.mu2e.MuStopPileupCat.${MIXCAMP}.art"`
-nevts=`samCountEvents.sh "dts.mu2e.MuStopPileupCat.${MIXCAMP}.art"`
+nfiles=`samCountFiles.sh "dts.mu2e.MuStopPileupCat.${MIXINCONF}.art"`
+nevts=`samCountEvents.sh "dts.mu2e.MuStopPileupCat.${MIXINCONF}.art"`
 let nskip_MuStopPileup=nevts/nfiles
 # write the mix.fcl
 rm -f mix.fcl
 # create a template file, starting from the basic Mix
 echo '#include "Production/JobConfig/mixing/Mix.fcl"' >> mix.fcl
 # locate the primary collection
-samweb list-file-locations --schema=root --defname="dts.mu2e.${PRIMARY}.${PCAMP}.art"  | cut -f1 > ${PRIMARY}.txt
+samListLocations ${SAMOPT} --defname="dts.mu2e.${PRIMARY}.${PRIMARYCONF}.art" > ${PRIMARY}.txt
 
 # Setup the beam intensity model
-if [ ${NBB} == "1BB" ]; then
+if [ ${PBEAM} == "1BB" ]; then
   echo '#include "Production/JobConfig/mixing/OneBB.fcl"' >> mix.fcl
-elif [ ${NBB} == "2BB" ]; then
+elif [ ${PBEAM} == "2BB" ]; then
   echo '#include "Production/JobConfig/mixing/TwoBB.fcl"' >> mix.fcl
-elif [ ${NBB} == "Low" ]; then
+elif [ ${PBEAM} == "Low" ]; then
   echo '#include "Production/JobConfig/mixing/LowIntensity.fcl"' >> mix.fcl
-elif [ ${NBB} == "Seq" ]; then
+elif [ ${PBEAM} == "Seq" ]; then
   echo '#include "Production/JobConfig/mixing/NoPrimaryPBISequence.fcl"' >> mix.fcl
 else
-  echo "Unknown proton beam intensity ${NBB}; aborting"
-  return 1;
+  echo "Unknown proton beam intensity ${PBEAM}; aborting"
+  exit_abnormal
 fi
 # setup option for early digitization
 if [ "${EARLY}" == "Early" ]; then
@@ -168,29 +221,30 @@ echo physics.filters.MuStopPileupMixer.mu2e.MaxEventsToSkip: ${nskip_MuStopPileu
 echo services.DbService.purpose: ${DBPURPOSE} >> mix.fcl
 echo services.DbService.version: ${DBVERSION} >> mix.fcl
 echo services.DbService.verbose : 2 >> mix.fcl
+echo "services.GeometryService.bFieldFile : \"${FIELD}\"" >> mix.fcl
 # overwrite the outputs
-echo outputs.SignalOutput.fileName: \"dig.owner.${MIXOUT}Signal.version.sequencer.art\" >> mix.fcl
-echo outputs.DiagOutput.fileName: \"dig.owner.${MIXOUT}Diag.version.sequencer.art\" >> mix.fcl
-echo outputs.TrkOutput.fileName: \"dig.owner.${MIXOUT}Trk.version.sequencer.art\" >> mix.fcl
-echo outputs.CaloOutput.fileName: \"dig.owner.${MIXOUT}Calo.version.sequencer.art\" >> mix.fcl
-echo outputs.UntriggeredOutput.fileName: \"dig.owner.${MIXOUT}Untriggered.version.sequencer.art\" >> mix.fcl
+echo outputs.SignalOutput.fileName: \"dig.owner.${OUTDESC}Signal.version.sequencer.art\" >> mix.fcl
+echo outputs.DiagOutput.fileName: \"dig.owner.${OUTDESC}Diag.version.sequencer.art\" >> mix.fcl
+echo outputs.TrkOutput.fileName: \"dig.owner.${OUTDESC}Trk.version.sequencer.art\" >> mix.fcl
+echo outputs.CaloOutput.fileName: \"dig.owner.${OUTDESC}Calo.version.sequencer.art\" >> mix.fcl
+echo outputs.UntriggeredOutput.fileName: \"dig.owner.${OUTDESC}Untriggered.version.sequencer.art\" >> mix.fcl
 
 # run generate_fcl
-generate_fcl --dsconf="${OUTCONF}" --dsowner=${OWNER} --description="${MIXOUT}" --embed mix.fcl \
+generate_fcl --dsconf="${OUTCONF}" --dsowner=${OWNER} --description="${OUTDESC}" --embed mix.fcl \
   --inputs="${PRIMARY}.txt" --merge-factor=${MERGE} \
-  --auxinput=${MUSTOPNMIXIN}:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat${MIXCAMP}.txt \
-  --auxinput=${ELENMIXIN}:physics.filters.EleBeamFlashMixer.fileNames:EleBeamFlashCat${MIXCAMP}.txt \
-  --auxinput=${MUBEAMNMIXIN}:physics.filters.MuBeamFlashMixer.fileNames:MuBeamFlashCat${MIXCAMP}.txt \
-  --auxinput=${NEUTNMIXIN}:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat${MIXCAMP}.txt
+  --auxinput=${MUSTOPNMIXIN}:physics.filters.MuStopPileupMixer.fileNames:MuStopPileupCat${MIXINCONF}.txt \
+  --auxinput=${ELENMIXIN}:physics.filters.EleBeamFlashMixer.fileNames:EleBeamFlashCat${MIXINCONF}.txt \
+  --auxinput=${MUBEAMNMIXIN}:physics.filters.MuBeamFlashMixer.fileNames:MuBeamFlashCat${MIXINCONF}.txt \
+  --auxinput=${NEUTNMIXIN}:physics.filters.NeutralsFlashMixer.fileNames:NeutralsFlashCat${MIXINCONF}.txt
 #  move to an appropriate directory
 for dirname in 000 001 002 003 004 005 006 007 008 009; do
   if test -d $dirname; then
     echo "found dir $dirname"
-    if test -d ${MIXOUT}_${dirname}; then
-      echo "removing ${MIXOUT}_${dirname}"
-      rm -rf "${MIXOUT}_${dirname}"
+    if test -d ${OUTDESC}_${dirname}; then
+      echo "removing ${OUTDESC}_${dirname}"
+      rm -rf "${OUTDESC}_${dirname}"
     fi
-    echo "moving $dirname to ${MIXOUT}_${dirname}"
-    mv $dirname ${MIXOUT}\_$dirname
+    echo "moving $dirname to ${OUTDESC}_${dirname}"
+    mv $dirname ${OUTDESC}\_$dirname
   fi
 done
