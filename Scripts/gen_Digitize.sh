02df97a5c42c657af0155219841d3aae481384bb
sophieMu2e <sophie@fnal.gov>
2023-05-26 09:00:00 -0500

    fix for gen script

--- Scripts/gen_Digitize.sh
+++ Scripts/gen_Digitize.sh
@@ -104,7 +104,7 @@ if [[ ${CAMPAIGN} == ""  || ${PRIMARY} == "" || ${PRIMARY_VERSION} == "" || ${OU
   exit_abnormal
 fi
 
-PRIMARYCONF=${CAMPAIGN}${PRIMARY_VERSION}
+PRIMARYCONF=${CAMPAIGN}${PRIMARY_VERSION}_${DBPURPOSE}_${DBVERSION}
 
 # Test: run a test to check the SimJob for this campaign verion exists
 DIR=/cvmfs/mu2e.opensciencegrid.org/Musings/SimJob/${CAMPAIGN}${OUTPUT_VERSION}
