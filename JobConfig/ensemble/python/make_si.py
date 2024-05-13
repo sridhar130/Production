3cbac5884984ed85d8d6337d8b05a4ffb79afa88
sophieMu2e <sophie@fnal.gov>
2024-05-13 13:08:51 -0500

    moved scripts

--- JobConfig/ensemble/python/make_si.py
+++ JobConfig/ensemble/python/make_si.py
@@ -9,6 +9,8 @@ import subprocess
 
 verbose = 1
 
+max_events_per_subrun = 50000
+
 dirname = sys.argv[1]
 outpath = sys.argv[2]
 
@@ -61,51 +63,31 @@ mean_reco_events = {}
 filenames = {}
 current_file = {}
 
-# loop over each "signal"
 for signal in norms:
     print(signal)
     #FIXME starting and ending event
-    
-    # open file list from the config directory
+
     ffns = open(os.path.join(dirname,"filenames_%s" % signal))
-    
-    # add empty file list
     filenames[signal] = []
-    
-    # enter empty entry for current file
     current_file[signal] = 0
-    
-    # enter empty entry for starting event
     starting_event_num[signal] = [0,0,0]
 
-    # start counters
     reco_events = 0
     gen_events = 0
-    
-    # loop over files in list
     for line in ffns:
         print("at line ", line, "of ", signal)
-        
-        # find a given filename
         fn = line.strip()
         print("striped filename ",fn)
-        
-        # add this filename to the list of filenames
         filenames[signal].append(fn)
-        
-        # use ROOT to get the events in that file
         fin = ROOT.TFile(fn)
         te = fin.Get("Events")
 
         # determine total number of events surviving all cuts
         reco_events += te.GetEntries()
-        #print(" reco events ", te.GetEntries())
-        
+        print(" reco events ", te.GetEntries())
         # determine total number of events generated
         t = fin.Get("SubRuns")
-        
-        # things are slightly different for the Cosmics:
-        if signal == "CRYCosmic" or signal == "CORSIKACosmic":
+        if signal == "CRYCosmic":
             # find the right branch
             bl = t.GetListOfBranches()
             bn = ""
@@ -119,38 +101,29 @@ for signal in norms:
             # find the right branch
             bl = t.GetListOfBranches()
             bn = ""
-            # find number of generated events via the GenEventCount field:
             for i in range(bl.GetEntries()):
                 if bl[i].GetName().startswith("mu2e::GenEventCount"):
                     bn = bl[i].GetName()
             for i in range(t.GetEntries()):
                 t.GetEntry(i)
                 gen_events += getattr(t,bn).product().count()
-        #print("total gen events ",gen_events)
+        print("total gen events ",gen_events)
+        #print("ALL NAMES",filenames)
 
-    # mean is the normalized number of that event type as expected
     mean_gen_events = norms[signal]
-    if verbose == 1:
-      print("mean_reco_events",mean_gen_events,reco_events,float(gen_events))
-    
-    # factors in efficiency
-    mean_reco_events[signal] = mean_gen_events*reco_events/float(gen_events) 
-    if verbose == 1:
-      print(signal,"GEN_EVENTS:",gen_events,"RECO_EVENTS:",reco_events,"EXPECTED EVENTS:",mean_reco_events[signal])
-
-# poisson sampling:
+    print("mean_reco_events",mean_gen_events,reco_events,float(gen_events))
+    mean_reco_events[signal] = mean_gen_events*reco_events/float(gen_events) # factors in efficiency
+    # DEBUG ONLY
+    print(signal,"GEN_EVENTS:",gen_events,"RECO_EVENTS:",reco_events,"EXPECTED EVENTS:",mean_reco_events[signal])
+print("sum of means ",sum(mean_reco_events.values()))
 total_sample_events = ROOT.gRandom.Poisson(sum(mean_reco_events.values()))
-if verbose == 1:
-  print("TOTAL EXPECTED EVENTS:",sum(mean_reco_events.values()),"GENERATING:",total_sample_events)
+# DEBUG ONLY
+print("TOTAL EXPECTED EVENTS:",sum(mean_reco_events.values()),"GENERATING:",total_sample_events)
 
 # calculate the normalized weights for each signal
 weights = {signal: mean_reco_events[signal]/float(total_sample_events) for signal in mean_reco_events}
-if verbose == 1:
-  print("weights " , weights)
-
+print("weights " , weights)
 # generate subrun by subrun
-
-# open the SamplingInput template:
 fin = open(os.path.join(os.environ["MUSE_WORK_DIR"],"Production/JobConfig/ensemble/fcl/SamplingInput.fcl"))
 t = Template(fin.read())
 
@@ -158,21 +131,16 @@ subrun = 0
 num_events_already_sampled = 0
 problem = False
 
-# this parameter controls how many events per fcl file:
-max_events_per_subrun = 10000000
 while True:
-    # split into "subruns" as requested by the max_events_per_subrun parameter
     events_this_run = max_events_per_subrun
     if num_events_already_sampled + events_this_run > total_sample_events:
         events_this_run = total_sample_events - num_events_already_sampled
 
-    # loop over signals via weights. Add text based on weight and file names
     datasets = ""
     for signal in weights:
         datasets += "      %s: {\n" % (signal)
         datasets += "        fileNames : [\"%s\"]\n" % (filenames[signal][current_file[signal]])
         datasets += "        weight : %e\n" % (weights[signal])
-        # add information on starting event, useful when have multiple .fcl per run
         if starting_event_num[signal] != [0,0,0]:
             datasets += "        skipToEvent : \"%d:%d:%d\"\n" % (starting_event_num[signal][0],starting_event_num[signal][1],starting_event_num[signal][2])
         datasets += "      }\n"
@@ -187,46 +155,12 @@ while True:
     # put all the exact parameter values in the fcl file
     d["comments"] = "#livetime: %f\n#rue: %e\n#rup: %e\n#kmax: %f\n#dem_emin: %f\n#dep_emin: %f\n#tmin: %f\n#max_livetime: %f\n#run: %d\n" % (livetime,rue,rup,kmax,dem_emin,dep_emin,tmin,max_livetime,run)
 
-    # make the .fcl file for this subrun (subrun # d)
     fout = open(os.path.join(dirname,"SamplingInput_sr%d.fcl" % (subrun)),"w")
     fout.write(t.substitute(d))
     fout.close()
 
-    # make a log file
-    flog = open(os.path.join(dirname,"SamplingInput_sr%d.log" % (subrun)),"w")
-
-    # run the fcl file using mu2e -c
-    cmd = ["mu2e","-c",os.path.join(dirname,"SamplingInput_sr%d.fcl" % (subrun)),"--nevts","%d" % (events_this_run)]
-    p = subprocess.Popen(cmd,stdout=subprocess.PIPE,universal_newlines=True)
-    ready = False
-    # loop over output of the process:
-    for line in p.stdout:
-        # write the files to log file TODO - time this effort
-        flog.write(line)
-        print(line)
-        if "Dataset" in line.split() and "Counts" in line.split() and "fraction" in line.split() and "Next" in line.split():
-            ready = True
-            print("READY",ready)
-        if ready:
-            if len(line.split()) > 1:
-                signal = line.split()[0].strip()
-                if signal in starting_event_num:
-
-                    if "no more available" in line:
-                        starting_event_num[signal] = [0,0,0]
-                        current_file[signal] += 1
-                        if current_file[signal] >= len(filenames[signal]):
-                            print("SIGNAL",signal,"HAS RUN OUT OF FILES!",current_file[signal])
-                            problem = True
-                    else:
-                        new_run = int(line.strip().split()[-5])
-                        new_subrun = int(line.strip().split()[-3])
-                        new_event = int(line.strip().split()[-1])
-                        starting_event_num[signal] = [new_run,new_subrun,new_event]
-    p.wait()
-
     num_events_already_sampled += events_this_run
-    print("Job done, return code: %d processed %d events out of %d" % (p.returncode,num_events_already_sampled,total_sample_events))
+    
     if problem:
         print("Error detected, exiting")
         sys.exit(1)
--- JobConfig/ensemble/python/normalizations.py
+++ JobConfig/ensemble/python/normalizations.py
@@ -3,16 +3,15 @@ import ROOT
 import math
 import os
 
-# numbers from SU2020
-mean_PBI_low = 1.6e7
-mean_PBI_high = 3.9e7
-mean_PBI = mean_PBI_low*0.75 + mean_PBI_high*0.25
-onspill_dutyfactor = 0.323
+# general
+"""
+mean_PBI =  mean_PBI_low*0.75 + mean_PBI_high*0.25 # 2.175e7 protons per pulse
+npulses_per_s = 189898.75
+onspill_dutyfactor = 0.323 # for 1BB
 offspill_dutyfactor = 0.323
-ub_per_second = 1/1695e-9*onspill_dutyfactor
-POT_per_second = ub_per_second*mean_PBI
-POT_per_year = ub_per_second*mean_PBI*3.15e7
-
+ub_per_second = (1/1695e-9)*onspill_dutyfactor # 1.905e5
+POT_per_second = ub_per_second*mean_PBI #  TODO 4.1447e12 POT/s
+"""
 
 # get stopped rates from DB
 dbtool = DbService.DbTool()
@@ -21,7 +20,6 @@ args=["print-run","--purpose","MDC2020_best","--version","v1_1","--run","1200","
 dbtool.setArgs(args)
 dbtool.run()
 rr = dbtool.getResult()
-#print(rr)
 
 
 # get number of target muon stops:
@@ -36,6 +34,31 @@ for line in lines:
         target_stopped_mu_per_POT = rate * 1000 
 print(f"Final stops rate {target_stopped_mu_per_POT}")
 
+# get number of POTs in given livetime
+def livetime_to_pot(livetime, run_mode = '1BB'): #livetime in seconds
+    # numbers from SU2020 
+    # see https://github.com/Mu2e/su2020/blob/master/analysis/pot_normalization.org
+    NPOT = 0.
+    if(run_mode == '1BB'):
+      # 1BB
+      mean_PBI_low = 1.6e7
+      Tcycle = 1.33 # sec
+      onspill_dutyfactor = 0.323
+      POT_per_cycle = 4e12
+      onspill_time = onspill_dutyfactor*livetime
+      Ncycles = onspill_time/Tcycle
+      NPOT = Ncycles * POT_per_cycle
+    if(run_mode == '2BB'):
+      # 2BB
+      mean_PBI_high = 3.9e7
+      Tcycle = 1.4 #s
+      onspill_dutyfactor = 0.246
+      POT_per_cycle = 8e12
+      onspill_time = onspill_dutyfactor*livetime
+      Ncycles = onspill_time/Tcycle
+      NPOT = Ncycles * POT_per_cycle
+    return NPOT
+
 # get number of ipa muon stops:
 ipa_stopped_mu_per_POT = 1.0
 rate = 1.0
@@ -49,8 +72,8 @@ for line in lines:
 print(f"Final ipa stops rate {ipa_stopped_mu_per_POT}")
 
 # get CE normalization:
-def ce_normalization(livetime, rue):
-    POT = livetime_to_pot(livetime)
+def ce_normalization(livetime, rue, run_mode = '1BB'):
+    POT = livetime_to_pot(livetime, run_mode)
     captures_per_stopped_muon = 0.609 # for Al
     print(f"Expected CE's {POT * target_stopped_mu_per_POT * captures_per_stopped_muon * rue}")
     return POT * target_stopped_mu_per_POT * captures_per_stopped_muon * rue
@@ -58,13 +81,13 @@ def ce_normalization(livetime, rue):
 # get IPA Michel normalization:
 def ipaMichel_normalization(livetime):
     POT = livetime_to_pot(livetime)
-    IPA_decays_per_stopped_muon = 0.92 # carbon
+    IPA_decays_per_stopped_muon = 0.92 # carbon....#TODO
     print(f"Expected IPA Michel e- {POT * ipa_stopped_mu_per_POT * IPA_decays_per_stopped_muon}")
     return POT * ipa_stopped_mu_per_POT * IPA_decays_per_stopped_muon
 
 # get DIO normalization:
-def dio_normalization(livetime, emin):
-    POT = livetime_to_pot(livetime)
+def dio_normalization(livetime, emin, run_mode = '1BB'):
+    POT = livetime_to_pot(livetime, run_mode)
     # calculate fraction of spectrum generated
     spec = open(os.path.join(os.environ["MUSE_WORK_DIR"],"Production/JobConfig/ensemble/heeck_finer_binning_2016_szafron.tbl")) 
     energy = []
@@ -82,32 +105,69 @@ def dio_normalization(livetime, emin):
 
     DIO_per_stopped_muon = 0.391 # 1 - captures_per_stopped_muon
 
-    physics_events = POT * target_stopped_mu_per_POT * DIO_per_stopped_muon * livetime
+    physics_events = POT * target_stopped_mu_per_POT * DIO_per_stopped_muon
     print(f"Expected DIO {physics_events* cut_norm/total_norm}")
     return physics_events * cut_norm/total_norm
 
 
 # note this returns CosmicLivetime not # of generated events
-def cry_onspill_normalization(livetime):
+def cry_onspill_normalization(livetime, run_mode = '1BB'):
+    onspill_dutyfactor = 1.
+    if(run_mode == '1BB'):
+      # 1BB
+      onspill_dutyfactor = 0.323
+    if(run_mode == '2BB'):
+      # 2BB
+      onspill_dutyfactor = 0.246
     print(f"cosmics live time {livetime*onspill_dutyfactor}")
     return livetime*onspill_dutyfactor
 
 # note this returns CosmicLivetime not # of generated events
-def cry_offspill_normalization(livetime):
+def cry_offspill_normalization(livetime, run_mode = '1BB'):
+    offspill_dutyfactor = 1.
+    if(run_mode == '1BB'):
+      # 1BB
+      offspill_dutyfactor = 0.323
+    if(run_mode == '2BB'):
+      # 2BB
+      offspill_dutyfactor = 0.246
+    print(f"cosmics live time {livetime*offspill_dutyfactor}")
+    return livetime*offspill_dutyfactor
+    
+# note this returns CosmicLivetime not # of generated events
+def corsika_onspill_normalization(livetime, run_mode = '1BB'):
+    onspill_dutyfactor = 1.
+    if(run_mode == '1BB'):
+      # 1BB
+      onspill_dutyfactor = 0.323
+    if(run_mode == '2BB'):
+      # 2BB
+      onspill_dutyfactor = 0.246
+    print(f"cosmics live time {livetime*onspill_dutyfactor}")
+    return livetime*onspill_dutyfactor
+
+# note this returns CosmicLivetime not # of generated events
+def corsika_offspill_normalization(livetime, run_mode = '1BB'):
+    offspill_dutyfactor = 1.
+    if(run_mode == '1BB'):
+      # 1BB
+      offspill_dutyfactor = 0.323
+    if(run_mode == '2BB'):
+      # 2BB
+      offspill_dutyfactor = 0.246
     print(f"cosmics live time {livetime*offspill_dutyfactor}")
     return livetime*offspill_dutyfactor
 
-def livetime_to_pot(livetime): #livetime in seconds
-    return livetime * POT_per_second
 
 def pot_to_livetime(pot):
     return pot / POT_per_second
 
 # for testing only
 if __name__ == '__main__':
-    livetime = pot_to_livetime(3.5e6)
-    ce_normalization(livetime, 1e-14)
+    livetime = pot_to_livetime(4e14) # 100s
+    print("testing livetime",livetime)
+    ce_normalization(livetime, 1e-13)
     ipaMichel_normalization(livetime/1.1e7)
     dio_normalization(livetime,75)
     cry_onspill_normalization(livetime)
-
+    corsika_onspill_normalization(livetime)
