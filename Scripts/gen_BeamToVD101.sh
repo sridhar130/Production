792821c7beb9fae7723a7ea427b72d51196e149b
PawelPlesniak <plesniakpaul@gmail.com>
2024-06-01 05:13:26 -0500

    Files required to perform STM simulation studies from EleBeamCat and MuBeamCat datasets to STMDet

---
+++ Scripts/gen_BeamToVD101.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+# Generate fcl files for resampling the beam up to VD101. Generates two sets of fcl files found in directories Ele_00X and Mu_00X
+# Pawel Plesniak
+
+# $1 is the production (ie MDC2020)
+# $2 is the input production version
+# $3 is the output production version
+# $4 is the number of events per job for electrons
+# $5 is the number of jobs for electrons
+# $6 is the number of events per job for muons
+# $7 is the number of jobs for muons
+
+if [[ ${7} == "" ]]; then
+  echo "Missing arguments!"
+  return -1
+fi
+
+# Generate the dataset list for electrons
+eleDataset=sim.mu2e.EleBeamCat.$1$2.art
+if [ -f EleBeamCat.txt ]; then
+    rm -f EleBeamCat.txt
+fi
+samweb list-file-locations --schema=root --defname="$eleDataset"  | cut -f1 > EleBeamCat.txt
+nEleFiles=`samCountFiles.sh $eleDataset`
+nEleEvts=`samCountEvents.sh $eleDataset`
+nEleSkip=$((nEleEvts/nEleFiles))
+echo "Electrons: found $nEleEvts in $nEleFiles, skipping max of $nEleSkip events per job"
+# Write beamToVD101Resampler.fcl for electrons
+if [ -f beamToVD101Resampler.fcl ]; then
+    rm -f beamToVD101Resampler.fcl
+fi
+echo '#include "Production/JobConfig/pileup/BeamToVD101.fcl"' >> beamToVD101Resampler.fcl
+echo physics.filters.beamResampler.mu2e.MaxEventsToSkip: ${nEleSkip} >> beamToVD101Resampler.fcl
+# Generate the electrons fcl files
+generate_fcl --dsconf=$1$3 --dsowner=$USER --run-number=1204 --description=BeamToVD101EleResampler --events-per-job=$4 --njobs=$5 \
+  --embed beamToVD101Resampler.fcl --auxinput=1:physics.filters.beamResampler.fileNames:EleBeamCat.txt 
+# Write the files to the correct directories
+for dirname in 000 001 002 003 004 005 006 007 008 009; do
+ if test -d $dirname; then
+  echo "found dir $dirname"
+  rm -rf Ele_$dirname
+  mv $dirname Ele_$dirname
+ fi
+done
+
+# Generate the dataset list for muons
+muDataset=sim.mu2e.MuBeamCat.$1$2.art
+if [ -f MuBeamCat.txt ]; then
+    rm -f MuBeamCat.txt
+fi
+samweb list-file-locations --schema=root --defname="$muDataset"  | cut -f1 > MuBeamCat.txt
+nMuFiles=`samCountFiles.sh $muDataset`
+nMuEvts=`samCountEvents.sh $muDataset`
+nMuSkip=$((nMuEvts/nMuFiles))
+echo "Muons: found $nMuEvts in $nMuFiles, skipping max of $nMuSkip events per job"
+# Write beamToVD101Resampler.fcl for muons
+rm -f beamToVD101Resampler.fcl
+echo '#include "Production/JobConfig/pileup/BeamToVD101.fcl"' >> beamToVD101Resampler.fcl
+echo physics.filters.beamResampler.mu2e.MaxEventsToSkip: ${nMuSkip} >> beamToVD101Resampler.fcl
+# Generate the electrons fcl files
+generate_fcl --dsconf=$1$3 --dsowner=$USER --run-number=1205 --description=BeamToVD101MuResampler --events-per-job=$6 --njobs=$7 \
+  --embed beamToVD101Resampler.fcl --auxinput=1:physics.filters.beamResampler.fileNames:MuBeamCat.txt 
+# Write the files to the correct directories
+for dirname in 000 001 002 003 004 005 006 007 008 009; do
+ if test -d $dirname; then
+  echo "found dir $dirname"
+  rm -rf Mu_$dirname
+  mv $dirname Mu_$dirname
+ fi
+done
+
+# Cleanup
+echo "Removing produced files"
+rm -f beamToVD101Resampler.fcl
+rm -f EleBeamCat.txt
+rm -f MuBeamCat.txt
+echo "Finished"
---
+++ Scripts/gen_STMResampler.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/bash
+# Generate fcl files for resampling the hits generated with BeamToVD101.fcl
+# Pawel Plesniak
+
+# $1 is the production (ie MDC2020)
+# $2 is the input production version
+# $3 is the output production version
+# $4 is the number of jobs
+# $5 is the project name of the BeamToVD101.fcl grid job. If not given, then it is assumed that the text file containing all the input datasets
+#    are listed in a file called STMDatasetList.txt with a single line at the end containing the number of events in the named STMDatasetList.txt.
+#    This works under the assumption that all jobs in the output directory are associated with the BeamToVD101.fcl grid job
+
+if [[ ${4} == "" ]]; then
+  echo "Missing arguments!"
+  return -1
+fi
+
+# TODO BEFORE NEXT CAMPAIGN - rewrite this to use the SAM tools samCountFiles.sh and samCountEvents.sh
+# Create the input dataset list STMDatasetList.txt
+if [[ ${5} != "" ]]; then
+  jobDir="/pnfs/mu2e/scratch/users/"$USER"/workflow/"$5"/outstage/*/*/*/*.art"
+  echo "Generating STMDatasetList.txt"
+  if [ -f STMDatasetList.txt ]; then
+    echo "Removing pre-existing STMDatasetList.txt"
+    rm STMDatasetList.txt;
+  fi
+  echo "Finding all the input datasets"
+  echo "Note - this is likely to take approx one minute."
+  find $jobDir -type f -name "*.art" > STMDatasetList.txt;
+  echo "Running Offline/Print/fcl/count.fcl -S STMDatasetList.txt"
+  echo "Note - this is likely to take a few mins especially if there are a lot of files"
+  tail=`mu2e -c Offline/Print/fcl/count.fcl -S STMDatasetList.txt | tail -n 30 | grep "Event records" | xargs`
+  tailArray=($tail)
+  nEvents=${tailArray[0]}
+  echo ${nEvents} >> STMDatasetList.txt
+  echo "Finished generating STMDatasetList.txt"
+else
+  echo "Using existing STMDatasetList.txt"
+  if [ ! -f STMDatasetList.txt ]; then
+      echo "No dataset list file called STMDatasetList.txt, exiting"
+      exit 1
+  fi
+  nEvents=`tail -n 1 STMDatasetList.txt | xargs`
+fi
+
+# Count the number of jobs and files
+nFiles=`wc -l STMDatasetList.txt`
+nFiles=($nFiles)
+nFiles=$((nFiles[0]-1))
+nSkip=$((nEvents/nFiles))
+echo "Found $nEvents events over $nFiles files. Setting MaxEventsToSkip as $nSkip"
+
+# Create a copy of STMDatasetList.txt without the event count at the end
+if [ -f STMDatasetListNoCount.txt ]; then
+  echo "Removing pre-existing STMDatasetListNoCount.txt"
+  rm -f STMDatasetListNoCount.txt
+fi
+head -n ${nFiles} STMDatasetList.txt > STMDatasetListNoCount.txt
+
+# write the template fcl
+rm -f stmResampler.fcl
+echo '#include "Production/JobConfig/pileup/STMResampler.fcl"' >> stmResampler.fcl
+echo physics.filters.stmResampler.mu2e.MaxEventsToSkip: ${nSkip} >> stmResampler.fcl
+
+generate_fcl --dsconf=$1$3 --dsowner=plesniak --run-number=1404 --description=STMResampler --events-per-job=20000000000 --njobs=$4 \
+  --embed stmResampler.fcl --auxinput=1:physics.filters.stmResampler.fileNames:STMDatasetListNoCount.txt # --old-seeds=seeds.plesniak.EleBeamResamplerSTM.MDC2020ab.CkMv.txt
+
+for dirname in 000 001 002 003 004 005 006 007 008 009; do
+ if test -d $dirname; then
+  echo "found dir $dirname"
+  rm -rf STMResampler_$dirname
+  mv $dirname STMResampler_$dirname
+ fi
+done
+
+# Cleanup
+echo "Removing temporary files"
+rm -f STMDatasetListNoCount.txt
+rm -f stmResampler.fcl
+echo "Finished"
