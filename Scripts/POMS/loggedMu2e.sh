3d0c82e4f30ae2fcf14e171b4526fb4e3550ffe3
Yuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2024-08-11 15:05:17 -0500

    Various tweaks to Production scripts

--- Scripts/POMS/loggedMu2e.sh
+++ Scripts/POMS/loggedMu2e.sh
@@ -2,7 +2,7 @@
 echo mu2e "$@"
 pwd
 ls
-filename=`basename ${@: -1}` 
+filename=`basename ${@: -1}`
 echo $filename
 logFilename=${filename/cnf./log.}
 logExtension=${logFilename/.fcl/.txt}
@@ -10,6 +10,6 @@ tbzFilename=${filename/cnf./bck.}
 tbzExtension=${tbzFilename/.fcl/.tbz}
 mu2e "$@" |& tee ${logExtension}
 mu2e_exit_status=${PIPESTATUS[0]}
-tar -cvjf ${tbzExtension} ${logExtension} ${logFilename}
+tar -cvjf ${tbzExtension} ${logExtension} ${filename}
 tar_exit_status=${?}
 exit ${mu2e_exit_status}
